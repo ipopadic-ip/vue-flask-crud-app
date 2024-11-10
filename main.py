f1083fd3197ae4de24f52d945bb8e965bf952ad2
Ilija <83288042+ipopadic-ip@users.noreply.github.com>
2024-11-10 22:12:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pymysql
 app = Flask(__name__, static_url_path="/")
 mysql = MySQL(app, cursorclass=pymysql.cursors.DictCursor)
 
-app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_USER"] = "***"
 app.config["MYSQL_DATABASE_PASSWORD"] = "***"
 app.config["MYSQL_DATABASE_DB"] = "prodavnica"
 
